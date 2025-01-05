8044e4d7e4e3954c912004e2f2a39f71dec00268
halfdane <halfdane@gmx.net>
2025-01-05 14:27:12 +0100

    automatic holding  from the  touchpad has interferred with keyboard mouse buttons. Reverting everything  to simply support tap gestures.

--- particle/kmk/README.md
+++ particle/kmk/README.md
@@ -2,7 +2,16 @@ Cirquitpython-based firmware for duality/particle
 
 Using kmk, it's supporting the integrated touchpad (with automagic layer switching) as well as most of miryoku.
 
-I wasn't *quite* able to get the timings for homerow mods and layer switches from the thumb keys to satisfaction.
+There are some notable deviations from miryoku, however: 
+- Thumb key combos are global, not dependent on the current layer
+- Tap both left thumb keys: ESCAPE
+- Hold both left thumb keys: MOUSE-Layer
+- Tap both right thumb keys: DELETE
+- Hold both right thumb keys: FN-Layer
 
 There's no need to build it, just copy it over after installing cirquitpython on the device.
 Or run `make` to copy over the necessary files from kmk as well.
+
+## Development
+Usually circuitpython will offer the device for mounting as a USB mass storage - this is disabled in the boot.py.
+To mount the board, keep a thumb key pressed on the side with the USB cable while the board boots (plug out, then in).
--- particle/kmk/lib/tapDetector.py
+++ particle/kmk/lib/tapDetector.py
@@ -8,54 +8,19 @@ class TapDetector:
         self.keyboard = keyboard
         self.tap_timeout = tap_timeout
         self.tap_timer = None
-        self.drag_timer = None
-        self.dragging = False
 
     def touch_start(self):
-        self._mouse_up("starting over")
-        if self.drag_timer:
-            self.keyboard.cancel_timeout(self.drag_timer)
-            self.drag_timer = None
-            self.dragging = True
-            self._mouse_down("drag start", 1)
-        else:
-            self.tap_timer = self.keyboard.set_timeout(self.tap_timeout, self._tap_timeout)
-            
+        self.tap_timer = self.keyboard.set_timeout(self.tap_timeout, self._tap_timeout)
 
     def touch_end(self):
-        if self.dragging:
-            self.dragging = False
-            # self._mouse_up("drag finished")
-            self.drag_timer = self.keyboard.set_timeout(self.tap_timeout, self._drag_timeout)
-        
-        elif self.tap_timer:
+        if self.tap_timer:
             self.keyboard.cancel_timeout(self.tap_timer)
             self.tap_timer = None
-            self._mouse_down("drag / tap")
-            self.drag_timer = self.keyboard.set_timeout(self.tap_timeout, self._drag_timeout)
-        
+            self.keyboard.tap_key(KC.MB_LMB)
 
     def is_tapping(self):
-        return self.tap_timer is not None or self.drag_timer is not None 
+        return self.tap_timer is not None
 
     def _tap_timeout(self):
         self.tap_timer = None
-        debug("Tap timed out")
-
-    def _drag_timeout(self):
-        self.drag_timer = None
-        self._mouse_up("drag was tap")
-
-    def _mouse_up(self, message, timeout=0):
-        def handler():
-            self.keyboard.remove_key(KC.MB_LMB)
-            debug(f"mouse up {message }")
-        return  self.keyboard.set_timeout(timeout, handler)
-  
-    def _mouse_down(self, message, timeout=0):
-        def handler():
-            self.keyboard.add_key(KC.MB_LMB)
-            debug(f"mouse down {message }")
-        return  self.keyboard.set_timeout(timeout, handler)
-  
 
